3a9eea7469e5e445cf4546ccc51559a1076f17ce
Перевалов Антон <paspean@mail.ru>
2024-08-09 22:50:53 +0700

    start app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+
+function App() {
+  return (
+    <>
+      <div>gefgegse</div>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </>
+  );
+}
+
+export default App;
